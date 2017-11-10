82c46f90ee3601d1dde4601643be8b065cf85003
Daisy Luna <daisycluna@gmail.com>
2017-11-10 20:33:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ let randomNumber = Math.floor(Math.random() * 20) + 1;
 
 let randomInteger = Math.floor(randomNumber);
 
-let randomUserID += oldID = randomInteger;
+let randomUserID + Math.floor(randomNumber) = oldID;
